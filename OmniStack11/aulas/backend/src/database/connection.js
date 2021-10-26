5ad5947b88723417fe6dbace0fdbc05f190ef6a3
anderson <anderson.pablo02@gmail.com>
2021-10-25 21:14:11 -0300

    refatoração

--- OmniStack11/aulas/backend/src/database/connection.js
+++
@@ -1,8 +0,0 @@
-const knex = require('knex');
-const configuration = require('../../knexfile');
-
-const config = process.env.NODE_ENV === 'test' ? configuration.test : configuration.development;
-
-const connnection = knex(config);
-
-module.exports = connnection;
--- OmniStack11/aulas/backend/src/server.js
+++
@@ -1,5 +0,0 @@
-const app = require('./app');
-
-app.listen(3333, function(){
-  console.log("Servidor rodando");
-});
